ec392b5587130345a43488bbf5908664f89e85ca
anishrahman <anishsirajudheen24@gmail.com>
2026-07-10 17:12:29 +0530

    Add Amplify frontend URL to CORS allowed origins

--- eventpro/src/main/java/com/eventpro/config/SecurityConfig.java
+++ eventpro/src/main/java/com/eventpro/config/SecurityConfig.java
@@ -71,7 +71,10 @@ public class SecurityConfig {
     @Bean
    public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173")); // Vite dev server origin
+        configuration.setAllowedOrigins(List.of(
+                "http://localhost:5173",
+                "https://main.d1ruiakp5kaujl.amplifyapp.com/"
+        )); // Vite dev server origin
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
